cc0c540a11a72f1361a0004dfdfc689a3233af4a
Quantum-Logic-Leap <quantumlogicleap@gmail.com>
2025-01-08 19:56:19 +0530

    added students

--- index.js
+++ index.js
@@ -12,4 +12,13 @@ app.get("/songs",(req,res)=>{
     res.send(["1","2"])
 })
 
+app.get("/students",(req,res)=>{
+    res.send(["rajesh","kumar"])
+})
+
+
 app.listen(4444,()=>{console.log("server started")})
+
+//http , https , ip(127.0.0.1) , port(4444) , api name(/songs)
+
+//http://127.0.0.1:4444/songs
